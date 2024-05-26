f4d4e33a4395cc393656ed320e4fb898f8e805ee
Sathish Kumar <sathishrsk2001@gmail.com>
2024-05-26 16:40:24 +0530

    app js modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Home from './Components/Main_page/Home'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
    <Header />
    <Home />
+   <Footer />
    </>
   )
 }
